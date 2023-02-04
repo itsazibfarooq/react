b91d96c04b08fd1fdd5899da9599a0d8e6e9c3bf
azib <64292764+azibfarooq@users.noreply.github.com>
2023-02-04 19:17:47 +0500

    updated template

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import ReactDOM from "react-dom/client";
-import Navbar from './Navbar.js';
-import Main from './Main.js';
-import './app.css'
-
-function App(){
-  return (
-    <>
-      <Navbar />
-      <Main />
-    </>
-  )
-}
+import './app.css';
+import App from './app.js';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<App />);
