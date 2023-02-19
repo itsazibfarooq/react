f14ec696755b8c4e02734903c2350e457659fe0d
azib <64292764+azibfarooq@users.noreply.github.com>
2023-02-19 14:42:01 +0500

    template files

--- src/app.js
+++ src/app.js
@@ -1,28 +1,15 @@
-import React from 'react';
-import './index.css';
-import data from './list.js';
-import Card from './card.js';
-import Form from './form.js';
+import React from 'react'
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-
-export default function App(){
-  const [color, setColor] = React.useState(data.map(item => item.color));
-  
-  function toggle(id){
-    setColor(prevState => prevState.map((item, index) => index === id ? !item : item));
-  }
-
-  const cards = data.map((item, index) => {
-    return <Card key={index} {...item} id = {index} itemColor = {color[index]} function={toggle} />
-  })
-  
+function App() {
   return (
-    <>
-      <div className='container'>
-        {/* {cards} */}
-        <Form />
-      </div>
-    </>
+    <div>
+
+    </div>
   )
 }
+
+export default App
