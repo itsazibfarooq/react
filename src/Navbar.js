45ed7255d8745e54c848cd9073f893f495ef5028
azib <64292764+azibfarooq@users.noreply.github.com>
2023-02-19 12:11:00 +0500

    complete project

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
 function Navbar() {
+
+  const [show, handleShow] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    }
+
+  }, []);
+
+
   return (
-    <div className='navbar__container'>
-      NETFLIX
-    </div>
+    <div className={`nav ${show && 'nav__black'}`} >
+      <img
+        className='nav__logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'
+        alt='Netflix logo'
+      />
+
+      <img
+        className='nav__avatar'
+        src='https://i.pinimg.com/originals/96/20/08/962008bd0eb249e4d575363114cec835.jpg'
+        alt='user avatar'
+      />
+    </div >
   )
 }
 
--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,13 @@
+import movieTrailer from 'movie-trailer';
 import React from 'react'
+import YouTube from 'react-youtube';
 import axios from './axios.js';
 
 const imgUrl = 'https://image.tmdb.org/t/p/original/';
 
 function Row(props) {
   const [movies, setMovies] = React.useState([]);
+  const [trailerUrl, setTrailerUrl] = React.useState('');
 
   React.useEffect(() => {
     async function fetchData() {
@@ -15,6 +18,28 @@ function Row(props) {
     fetchData();
   }, [props.fetchUrl]);
 
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    }
+  };
+
+  const handleMovie = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    }
+    else {
+      movieTrailer(movie?.name || '')
+        .then(url => {
+          const urlParam = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParam.get('v'));
+        })
+        .catch(error => console.log(error));
+    }
+  };
+
   return (
     <div className='row'>
       <h2>{props.title}</h2>
@@ -22,6 +47,7 @@ function Row(props) {
       <div className='row__posters'>
         {movies.map((item) => (
           <img
+            onClick={() => handleMovie(item)}
             key={item.id}
             className={`row__poster ${props.isLargeRow && 'row__posterLarge'}`}
             src={`${imgUrl}${props.isLargeRow ? item.poster_path : item.backdrop_path}`}
@@ -29,6 +55,7 @@ function Row(props) {
           </img>
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
